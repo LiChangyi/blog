e48f987ab58c73f5284a513c2e917c7d9def5945
licy <licy97love@gmail.com>
2022-01-03 18:57:39 +0800

    feat: For the record

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="flex items-center px-6 justify-end" style="height: 60px">
+  <header class="flex items-center px-6 justify-between" style="height: 60px">
+    <div>代码多多</div>
     <nav>
       <router-link class="nav-item mr-4" to="/">Home</router-link>
       <router-link class="nav-item" to="/about">About</router-link>
