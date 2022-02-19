168c39b237f6790ebeb82646de213caf5975953e
licy <licy97love@gmail.com>
2022-02-19 11:23:29 +0800

    feat: remove name and fix typos

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <header class="flex items-center px-6 justify-between" style="height: 60px">
-    <div>代码多多</div>
+  <header class="flex items-center px-6 justify-end" style="height: 60px">
     <nav>
       <router-link class="nav-item mr-4" to="/">Home</router-link>
       <router-link class="nav-item" to="/about">About</router-link>
